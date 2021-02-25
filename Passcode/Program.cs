35296df180e63888e78d0bd99d7d9862f6ed0d42
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2021-02-25 02:21:07 +0100

    Add project files.

---
+++ Passcode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = "";
+
+            while (code != "secret" )
+            {
+                Console.WriteLine("Enter your log in");
+                code = Console.ReadLine();
+
+                if (code != "secret")
+                {
+                    Console.WriteLine("Try again");
+                }
+            }
+            Console.WriteLine("Successful");
+        }
+
+    }
+}
